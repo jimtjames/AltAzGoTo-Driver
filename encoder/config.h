039910335e5b6dece8a638656002d65c84aa8a62
Jim James <jimtjames@gmail.com>
2018-11-22 13:45:12 -0600

    Fix bugs relating to transformation matrix

--- encoder/config.h
+++ encoder/config.h
@@ -1,5 +1,5 @@
-#define ENCODER_ALT_A PB10 // encoder 1 green
-#define ENCODER_ALT_B PB11 // encoder 1 white
+#define ENCODER_ALT_A PA8 // encoder 1 green
+#define ENCODER_ALT_B PB12 // encoder 1 white
 #define ENCODER_AZ_A PB4 // encoder 2 white
 #define ENCODER_AZ_B PA15 // encoder 2 green
 #define RATIO_ALT 15 //Amount of encoder turns to bearing turns
